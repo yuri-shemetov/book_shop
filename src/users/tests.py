f0ca05124718e6e742bfa48f8ae13a8e86cad6e3
Yuri Shemetov <yuri-sh@tut.by>
2021-07-14 21:29:43 +0300

    My bookstore v.1.1

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class ViewTest(TestCase):
+    def setUp(self):
+        User.objects.create_user(username="dummy", password="1234567!")
+
+    def test_user_auth_login(self):
+        response = self.client.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_auth_register(self):
+        response = self.client.get(reverse('users:register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_auth_user(self):
+        self.client.login(username="dummy", password="1234567!")
+        response = self.client.get(reverse('users:user'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_no_auth_user(self):
+        response = self.client.get(reverse('users:user'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_auth_users_all(self):
+        self.client.login(username="dummy", password="1234567!")
+        response = self.client.get(reverse('users:users-all'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_no_auth_users_all(self):
+        response = self.client.get(reverse('users:users-all'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_no_auth_other_user(self):
+        response = self.client.get(reverse('users:other-user'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_no_auth_detail(self):
+        response = self.client.get(reverse('users:user-detail'))
+        self.assertEqual(response.status_code, 302)
+
+
